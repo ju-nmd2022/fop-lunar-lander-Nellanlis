eb80568f34d1ef885f65890bdde99c201f0b6a72
Nellanlis <117353226+Nellanlis@users.noreply.github.com>
2023-02-17 12:46:45 +0100

    making the stars slowly appear

--- starsky.js
+++ starsky.js
@@ -1,12 +1,12 @@
 let particles = [];
 
-fill(145, 145, 145);
-rect(0, 450, 100, 100);
+//stars in the sky
+background(0, 0, 0);
 
 push();
 function createParticle() {
   const x = Math.random() * width;
-  const y = Math.random() * height;
+  const y = Math.random() * (height - 140);
   return { x: x, y: y };
 }
 
@@ -14,9 +14,16 @@ function drawParticle(particle) {
   push();
   translate(particle.x, particle.y);
   noStroke();
-  fill(255, 255, 255, 100);
-  ellipse(0, 0, 1.4);
+  fill(255, 255, 255, 3);
+  ellipse(0, 0, 1.5);
   pop();
+}
+function joinParticle(particle) {
+  let dis = dist(particle.x, particle.y);
+  if (dis < 40) {
+    stroke(255, 255, 255, 0.04);
+    line(particle.x, particle.y);
+  }
 }
 
 function updateParticle(particle) {
@@ -24,16 +31,19 @@ function updateParticle(particle) {
   particle.y = particle.y;
 }
 
-for (let i = 100; i < 200; i++) {
+for (let i = 100; i < 500; i++) {
   const particle = createParticle();
   particles.push(particle);
 }
 
 function draw() {
-  background(0, 0, 0);
   for (let particle of particles) {
     drawParticle(particle);
     updateParticle(particle);
+    joinParticle(particle.slice(i));
   }
 }
 pop();
+
+fill(145, 145, 145);
+rect(0, 450, 600, 120);
