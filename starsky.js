1526aa9f1d87284037e795f55e4070409bfba2d5
Nellanlis <117353226+Nellanlis@users.noreply.github.com>
2023-03-06 17:24:53 +0100

    pretty much finisheddd
    Updated some variables and added new ones because of issues

--- starsky.js
+++ starsky.js
@@ -1,33 +1,29 @@
 // the spaceship
 let x = 300;
-let y = 300;
+let y = 50;
 let s = 0.5;
 let meteorX = 300;
 let meteorY = 300;
+let satelliteX = 300;
+let satelliteY = 300;
+let biggerX = 300;
+let biggerY = 300;
+let biggerS = 2;
 
 //start button screen
 function startScreen() {
-  push();
+  fill(255, 255, 255);
   noStroke();
-  let col = color(255, 255, 255);
-  let colo = color(100, 100, 100);
-  let startButton = createButton("Start Game");
-  startButton.style("stroke", "5");
-  startButton.style("background-color", colo);
-  startButton.style("font-size", "27px");
-  startButton.style("color", col);
-  startButton.style("border", col);
-  startButton.style("width", "150px");
-  startButton.position(width / 2 - 60, height / 2 - 50);
-  pop();
+  textSize(50);
+  text("Press SPACE", 150, 230);
+  textSize(40);
+  text("to start the game", 155, 295);
 
-  //moon
-  fill(0, 0, 0, 60);
-  rect(x - 300, y + 150, width, 150);
   fill(255, 255, 255);
   textSize(14);
   text("land the spaceship slowly and securely on the moon", 145, 350);
-  text("click the space button to move the spaceship upwards", 140, 380);
+  text("hold down the space button to move the spaceship upwards", 120, 380);
+  planets();
 }
 
 function meteor() {
@@ -88,58 +84,72 @@ function moon() {
 function satellite() {
   noStroke();
   push();
-  translate(x - 300, y - 260);
+  translate(satelliteX - 300, satelliteY - 260);
   rotate(-0.09999);
 
   //satellite body
   fill(255, 255, 255);
   stroke(255, 255, 255);
   beginShape();
-  vertex(x + 150, y - 150);
-  bezierVertex(x + 150, y - 160, x + 170, y - 160, x + 170, y - 150);
-  vertex(x + 170, y - 130);
-  bezierVertex(x + 170, y - 120, x + 150, y - 120, x + 150, y - 130);
+  vertex(satelliteX + 150, satelliteY - 150);
+  bezierVertex(
+    satelliteX + 150,
+    satelliteY - 160,
+    satelliteX + 170,
+    satelliteY - 160,
+    satelliteX + 170,
+    satelliteY - 150
+  );
+  vertex(satelliteX + 170, satelliteY - 130);
+  bezierVertex(
+    satelliteX + 170,
+    satelliteY - 120,
+    satelliteX + 150,
+    satelliteY - 120,
+    satelliteX + 150,
+    satelliteY - 130
+  );
   endShape(CLOSE);
 
   //top orange bit
   fill(255, 95, 0);
   noStroke();
-  ellipse(x + 160, y - 153, 20, 10);
+  ellipse(satelliteX + 160, satelliteY - 153, 20, 10);
   fill(255, 150, 0);
-  ellipse(x + 160, y - 153, 13, 6);
+  ellipse(satelliteX + 160, satelliteY - 153, 13, 6);
 
   //ball on top
   fill(0, 191, 255);
-  ellipse(x + 160, y - 161, 16);
-  ellipse(x + 160, y - 143, 6);
+  ellipse(satelliteX + 160, satelliteY - 161, 16);
+  ellipse(satelliteX + 160, satelliteY - 143, 6);
   fill(255, 255, 0);
-  ellipse(x + 160, y - 135, 3);
+  ellipse(satelliteX + 160, satelliteY - 135, 3);
 
   //wings of satellite
   fill(50, 205, 50);
-  rect(x + 120, y - 150, 25, 20);
-  rect(x + 175, y - 150, 25, 20);
+  rect(satelliteX + 120, satelliteY - 150, 25, 20);
+  rect(satelliteX + 175, satelliteY - 150, 25, 20);
   fill(255, 255, 0);
-  rect(x + 124, y - 147, 6, 6);
-  rect(x + 124, y - 139, 6, 6);
-  rect(x + 132, y - 147, 6, 6);
-  rect(x + 132, y - 139, 6, 6);
-  rect(x + 179, y - 147, 6, 6);
-  rect(x + 179, y - 139, 6, 6);
-  rect(x + 187, y - 147, 6, 6);
-  rect(x + 187, y - 139, 6, 6);
+  rect(satelliteX + 124, satelliteY - 147, 6, 6);
+  rect(satelliteX + 124, satelliteY - 139, 6, 6);
+  rect(satelliteX + 132, satelliteY - 147, 6, 6);
+  rect(satelliteX + 132, satelliteY - 139, 6, 6);
+  rect(satelliteX + 179, satelliteY - 147, 6, 6);
+  rect(satelliteX + 179, satelliteY - 139, 6, 6);
+  rect(satelliteX + 187, satelliteY - 147, 6, 6);
+  rect(satelliteX + 187, satelliteY - 139, 6, 6);
   pop();
 }
 
 function planets() {
   noStroke();
   fill(255, 255, 255, 30);
-  ellipse(x + 45, y - 200, 25);
-  ellipse(x - 150, y - 100, 20);
+  ellipse(satelliteX + 45, satelliteY - 200, 25);
+  ellipse(satelliteX - 150, satelliteY - 100, 20);
   fill(255, 255, 255, 70);
-  ellipse(x + 140, y - 50, 10);
-  ellipse(x - 190, y - 140, 30);
-  ellipse(x + 250, y - 240, 30);
+  ellipse(satelliteX + 140, satelliteY - 50, 10);
+  ellipse(satelliteX - 190, satelliteY - 140, 30);
+  ellipse(satelliteX + 250, satelliteY - 240, 30);
 }
 
 function fire() {
@@ -231,7 +241,7 @@ function spaceship() {
   //yellow by fire
   fill(255, 255, 0);
   beginShape();
-  vertex(x + 30 * s, x + 60 * s);
+  vertex(x + 30 * s, y + 60 * s);
   vertex(x - 30 * s, y + 60 * s);
   vertex(x - 35 * s, y + 80 * s);
   bezierVertex(
@@ -311,42 +321,173 @@ function spaceship() {
   ellipse(x + s, y - 105 * s, s * 8);
 }
 
+function biggerSpaceship() {
+  //bigger spaceship for end screens
+
+  //yellow spacers
+  push();
+  translate(biggerX - 80 * biggerS, biggerY + 40 * biggerS);
+  rotate(2.5);
+  fill(255, 255, 0);
+  rect(-30 * biggerS, -20 * biggerS, 50 * biggerS, 30 * biggerS);
+  pop();
+
+  push();
+  translate(biggerX + 60 * biggerS, biggerY + 20 * biggerS);
+  rotate(0.7);
+  fill(255, 255, 0);
+  rect(0 * biggerS, -9 * biggerS, 50 * biggerS, 30 * biggerS);
+  pop();
+
+  //yellow by fire
+  fill(255, 255, 0);
+  beginShape();
+  vertex(biggerX + 30 * biggerS, biggerY + 60 * biggerS);
+  vertex(biggerX - 30 * biggerS, biggerY + 60 * biggerS);
+  vertex(biggerX - 35 * biggerS, biggerY + 80 * biggerS);
+  bezierVertex(
+    biggerX - 10 * biggerS,
+    biggerY + 75 * biggerS,
+    biggerX + 10 * biggerS,
+    biggerY + 75 * biggerS,
+    biggerX + 35 * biggerS,
+    biggerY + 80 * biggerS
+  );
+  endShape();
+
+  //body
+  fill(255, 16, 240);
+  ellipse(biggerX, biggerY, 150 * biggerS);
+
+  // feet
+  beginShape();
+  vertex(biggerX - 120 * biggerS, biggerY + 90 * biggerS);
+  bezierVertex(
+    biggerX - 120 * biggerS,
+    biggerY + 20 * biggerS,
+    biggerX - 80 * biggerS,
+    biggerY + 20 * biggerS,
+    biggerX - 80 * biggerS,
+    biggerY + 90 * biggerS
+  );
+  endShape();
+  beginShape();
+  vertex(biggerX + 120 * biggerS, biggerY + 90 * biggerS);
+  bezierVertex(
+    biggerX + 120 * biggerS,
+    biggerY + 20 * biggerS,
+    biggerX + 80 * biggerS,
+    biggerY + 20 * biggerS,
+    biggerX + 80 * biggerS,
+    biggerY + 90 * biggerS
+  );
+  endShape();
+
+  //window
+  fill(0, 191, 255);
+  ellipse(biggerX, biggerY, biggerS * 55);
+  fill(0, 255, 255);
+  ellipse(biggerX, biggerY, biggerS * 40);
+  fill(255, 255, 255);
+  ellipse(biggerX - 12 * biggerS, biggerY - 4 * biggerS, biggerS * 8);
+  ellipse(biggerX - 3 * biggerS, biggerY - 10 * biggerS, biggerS * 11);
+
+  //top of the spaceship
+  fill(100, 255, 0);
+  beginShape();
+  vertex(biggerX - 60 * biggerS, biggerY - 50 * biggerS);
+  bezierVertex(
+    biggerX - 20 * biggerS,
+    biggerY - 40 * biggerS,
+    biggerX + 20 * biggerS,
+    biggerY - 40 * biggerS,
+    biggerX + 60 * biggerS,
+    biggerY - 50 * biggerS
+  );
+  bezierVertex(
+    biggerX + 20 * biggerS,
+    biggerY - 100 * biggerS,
+    biggerX - 20 * biggerS,
+    biggerY - 100 * biggerS,
+    biggerX - 60 * biggerS,
+    biggerY - 50 * biggerS
+  );
+  endShape();
+
+  //antenna on top
+  fill(255, 255, 255);
+  stroke(255, 255, 255);
+  strokeWeight(4);
+  line(
+    biggerX + biggerS,
+    biggerY - 80 * biggerS,
+    biggerX + biggerS,
+    biggerY - 105 * biggerS
+  );
+  ellipse(biggerX + biggerS, biggerY - 105 * biggerS, biggerS * 8);
+}
+
 //astronaut guy sad
 function astronaut1() {
   fill(255, 255, 255);
   stroke(0, 0, 0);
   strokeWeight(1);
   //body
-  rect(x - 80 * s, y + 65 * s, 20 * s, 20 * s);
+  rect(
+    biggerX - 80 * biggerS,
+    biggerY + 65 * biggerS,
+    20 * biggerS,
+    20 * biggerS
+  );
   //head
-  ellipse(x - 70 * s, y + 51 * s, 40 * s);
+  ellipse(biggerX - 70 * biggerS, biggerY + 51 * biggerS, 40 * biggerS);
   //arms
-  ellipse(x - 82.5 * s, y + 75 * s, 7.5 * s, 15 * s);
-  ellipse(x - 57.5 * s, y + 75 * s, 7.5 * s, 15 * s);
+  ellipse(
+    biggerX - 82.5 * biggerS,
+    biggerY + 75 * biggerS,
+    7.5 * biggerS,
+    15 * biggerS
+  );
+  ellipse(
+    biggerX - 57.5 * biggerS,
+    biggerY + 75 * biggerS,
+    7.5 * biggerS,
+    15 * biggerS
+  );
   //feet
-  ellipse(x - 75 * s, y + 85 * s, 11 * s);
-  ellipse(x - 65 * s, y + 85 * s, 11 * s);
+  ellipse(biggerX - 75 * biggerS, biggerY + 85 * biggerS, 11 * biggerS);
+  ellipse(biggerX - 65 * biggerS, biggerY + 85 * biggerS, 11 * biggerS);
   noStroke();
   fill(200, 200, 200);
-  ellipse(x - 75 * s, y + 85 * s, 7.5 * s);
-  ellipse(x - 65 * s, y + 85 * s, 7.5 * s);
+  ellipse(biggerX - 75 * biggerS, biggerY + 85 * biggerS, 7.5 * biggerS);
+  ellipse(biggerX - 65 * biggerS, biggerY + 85 * biggerS, 7.5 * biggerS);
   //face
   fill(0, 0, 0);
-  ellipse(x - 70 * s, y + 60 * s, 30 * s, 15 * s);
+  ellipse(
+    biggerX - 70 * biggerS,
+    biggerY + 60 * biggerS,
+    30 * biggerS,
+    15 * biggerS
+  );
   fill(255, 255, 255);
-  ellipse(x - 80 * s, y + 57.5 * s, 5 * s, 2.5 * s);
+  ellipse(
+    biggerX - 80 * biggerS,
+    biggerY + 57.5 * biggerS,
+    5 * biggerS,
+    2.5 * biggerS
+  );
 
   //tear
   fill(0, 191, 255);
   beginShape();
-  vertex(x - 80 * s, y + 62.5 * s);
+  vertex(biggerX - 80 * biggerS, biggerY + 62.5 * biggerS);
   bezierVertex(
-    x - 90 * s,
-    y + 75 * s,
-    x - 70 * s,
-    y + 75 * s,
-    x - 80 * s,
-    y + 62.5 * s
+    biggerX - 90 * biggerS,
+    biggerY + 75 * biggerS,
+    biggerX - 70 * biggerS,
+    biggerY + 75 * biggerS,
+    biggerX - 80 * biggerS,
+    biggerY + 62.5 * biggerS
   );
   endShape();
 }
@@ -357,34 +498,84 @@ function astronaut2() {
   stroke(0, 0, 0);
   strokeWeight(1);
   //body
-  rect(x - 80 * s, y + 65 * s, 20 * s, 20 * s);
+  rect(
+    biggerX - 80 * biggerS,
+    biggerY + 65 * biggerS,
+    20 * biggerS,
+    20 * biggerS
+  );
   //head
-  ellipse(x - 70 * s, y + 51 * s, 40 * s);
+  ellipse(biggerX - 70 * biggerS, biggerY + 51 * biggerS, 40 * biggerS);
   //arms
-  ellipse(x - 86 * s, y + 71 * s, 15 * s, 7.5 * s);
-  ellipse(x - 57.5 * s, y + 75 * s, 7.5 * s, 15 * s);
+  ellipse(
+    biggerX - 86 * biggerS,
+    biggerY + 71 * biggerS,
+    15 * biggerS,
+    7.5 * biggerS
+  );
+  ellipse(
+    biggerX - 57.5 * biggerS,
+    biggerY + 75 * biggerS,
+    7.5 * biggerS,
+    15 * biggerS
+  );
   //feet
-  rect(x - 78.5 * s, y + 85 * s, 7.5 * s, 7.5 * s);
-  rect(x - 69 * s, y + 85 * s, 7.5 * s, 7.5 * s);
-  ellipse(x - 76.5 * s, y + 93 * s, 10 * s, 6 * s);
-  ellipse(x - 63 * s, y + 93 * s, 10 * s, 6 * s);
+  rect(
+    biggerX - 78.5 * biggerS,
+    biggerY + 85 * biggerS,
+    7.5 * biggerS,
+    7.5 * biggerS
+  );
+  rect(
+    biggerX - 69 * biggerS,
+    biggerY + 85 * biggerS,
+    7.5 * biggerS,
+    7.5 * biggerS
+  );
+  ellipse(
+    biggerX - 76.5 * biggerS,
+    biggerY + 93 * biggerS,
+    10 * biggerS,
+    6 * biggerS
+  );
+  ellipse(
+    biggerX - 63 * biggerS,
+    biggerY + 93 * biggerS,
+    10 * biggerS,
+    6 * biggerS
+  );
   //face
   noStroke();
   fill(0, 0, 0);
-  ellipse(x - 70 * s, y + 49.5 * s, 32.5 * s, 27.5 * s);
+  ellipse(
+    biggerX - 70 * biggerS,
+    biggerY + 49.5 * biggerS,
+    32.5 * biggerS,
+    27.5 * biggerS
+  );
   push();
-  translate(x - 80 * s, y + 45 * s);
+  translate(biggerX - 80 * biggerS, biggerY + 45 * biggerS);
   rotate(2.2);
   fill(255, 255, 255);
-  ellipse(0, 0, 9 * s, 5 * s);
+  ellipse(0, 0, 9 * biggerS, 5 * biggerS);
   pop();
   //flag
   noStroke();
   fill(100, 255, 0);
-  rect(x - 135 * s, y + 7.5 * s, 40 * s, 30 * s);
+  rect(
+    biggerX - 135 * biggerS,
+    biggerY + 7.5 * biggerS,
+    40 * biggerS,
+    30 * biggerS
+  );
   stroke(255, 255, 255);
   strokeWeight(3);
-  line(x - 92.5 * s, y + 80 * s, x - 92.5 * s, y + 5 * s);
+  line(
+    biggerX - 92.5 * biggerS,
+    biggerY + 80 * biggerS,
+    biggerX - 92.5 * biggerS,
+    biggerY + 5 * biggerS
+  );
 }
 
 function gameScreen() {
@@ -393,8 +584,8 @@ function gameScreen() {
   planets();
   if (keyIsDown(32)) {
     fire(x, y, 0.5);
-    spaceship(x, y, 0.5);
   }
+  spaceship(x, y, 0.5);
 }
 
 let state = "start";
@@ -432,11 +623,66 @@ function draw() {
 
   if (keyIsDown(32) && state === "start") {
     state = "game";
-  } else if (keyIsDown(32) && state === "game" && y < 400) {
+  } else if (keyIsDown(32) && state === "game" && y < 480) {
     isGameActive = true;
   } else if (keyIsDown(13) && (state === "lose" || state === "win")) {
     state = "game";
   }
+
+  // not linear
+  if (isGameActive) {
+    y = y + velocity;
+    velocity = velocity + acceleration;
+  }
+
+  //making it flyyyy
+  if (keyIsDown(32)) {
+    velocity = velocity - 0.4;
+  }
+
+  //where to land
+  if (y > 432) {
+    isGameActive = false;
+  }
+
+  if (velocity >= 1 && y >= 432) {
+    isGameActive = false;
+    state = "lose";
+    y = 50;
+    velocity = 1;
+  } else if (velocity < 1 && y >= 432) {
+    isGameActive = false;
+    state = "win";
+    y = 50;
+    velocity = 1;
+  }
+}
+function loseScreen() {
+  noStroke();
+  fill(50, 50, 50);
+  rect(0, 370, width, 200);
+  fill(100, 100, 100);
+  ellipse(300, 480, 550, 90);
+  biggerSpaceship();
+  astronaut1();
+  fill(255, 255, 255);
+  textSize(25);
+  text("You lost :(", 50, 80);
+  textSize(18);
+  text("press the enter key to start over", 30, 110);
 }
 
-// if mouseisclicked the fire should be y - 10 or something.Look at Liams repository
+function winScreen() {
+  noStroke();
+  fill(50, 50, 50);
+  rect(0, 370, width, 200);
+  fill(100, 100, 100);
+  ellipse(300, 480, 550, 90);
+  biggerSpaceship();
+  astronaut2();
+  fill(255, 255, 255);
+  textSize(25);
+  text("You win :)", 50, 80);
+  textSize(18);
+  text("press the enter key to start over", 30, 110);
+}
